e637a3c85894abde94544e2eab0bd6b1621f5f78
Jim Barritt <jimb@thoughtworks.com>
2017-02-16 21:01:32 +0000

    Got a JWT Token back

--- src/test/java/jmdb/oidc/api/TestOidcApi.java
+++ src/test/java/jmdb/oidc/api/TestOidcApi.java
@@ -2,12 +2,21 @@ package jmdb.oidc.api;
 
 import jmdb.oidc.platform.HttpTestBase;
 import jmdb.oidc.platform.http.client.HttpResponse;
+import jmdb.oidc.platform.http.client.JsonResponseHandler;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static jmdb.oidc.platform.http.client.Http.addBasicAuthzHeader;
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -19,13 +28,29 @@ public class TestOidcApi extends HttpTestBase {
     }
 
     @Test
-    public void can_initiate_auth_sequence() {
-        HttpGet get_wellKown = new HttpGet(url("/realms/%s/.well-known/openid-configuration", realmName));
+    public void can_initiate_auth_sequence() throws Exception {
+        HttpGet get_wellKnown = new HttpGet(url("/realms/%s/.well-known/openid-configuration", realmName));
 
-        HttpResponse welknownResponse = http.execute(get_wellKown);
+        HttpResponse wellknownResponse = http.execute(get_wellKnown);
 
-        assertThat(welknownResponse.statusCode(), is(HttpStatus.SC_OK));
-        System.out.println(welknownResponse);
+        assertThat(wellknownResponse.statusCode(), is(SC_OK));
+        System.out.println(wellknownResponse);
 
+        String token_endpoint = wellknownResponse.stringValue("token_endpoint");
+
+        HttpPost post_token = new HttpPost(token_endpoint);
+
+        List<NameValuePair> form_data = new ArrayList<>();
+        form_data.add(new BasicNameValuePair("client_id", "test-client"));
+        form_data.add(new BasicNameValuePair("client_secret", "6b0f7d59-283d-4afe-b13e-747158d84086"));
+        form_data.add(new BasicNameValuePair("username", "test_user"));
+        form_data.add(new BasicNameValuePair("password", "foo"));
+        form_data.add(new BasicNameValuePair("grant_type", "password"));
+        post_token.setEntity(new UrlEncodedFormEntity(form_data));
+
+        HttpResponse tokenResponse = http.execute(post_token);
+
+        assertThat(tokenResponse.statusCode(), is(SC_OK));
+        System.out.println(tokenResponse);
     }
 }
